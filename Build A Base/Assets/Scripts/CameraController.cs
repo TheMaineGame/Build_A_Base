adeb4ec1d07e2240a31637952bd59c2481308bd6
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2015-12-16 15:23:02 -0500

    Renamed building models
    For clarity and stuff

--- Build A Base/Assets/Scripts/CameraController.cs
+++ Build A Base/Assets/Scripts/CameraController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControll : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
     [SerializeField]
     GameObject cameraObject;
