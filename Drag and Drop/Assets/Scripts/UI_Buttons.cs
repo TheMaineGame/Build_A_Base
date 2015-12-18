4a2ac4cf3dee661ee7f4710fb1fe6a16777d704a
Alex Rediker <redikeralex@utc4me.org>
2015-12-18 15:57:56 -0500

    Alex's Menu NOT FINISHED YET
    HERE I BE

---
+++ Drag and Drop/Assets/Scripts/UI_Buttons.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Buttons : MonoBehaviour 
+{
+	[SerializeField]GameObject m_livingQuarters;
+
+	public void CubeCall () 
+	{
+		m_livingQuarters.SetActive (true);
+	}
+}
