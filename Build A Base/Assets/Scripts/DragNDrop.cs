7f7e41c8558c73f65a79797d26662b50d5ec64de
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-01-06 16:26:47 -0500

    Things

---
+++ Build A Base/Assets/Scripts/DragNDrop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragNDrop : MonoBehaviour {
+    GameObject Dragging;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButton(0)) {
+            
+        }
+	}
+}
