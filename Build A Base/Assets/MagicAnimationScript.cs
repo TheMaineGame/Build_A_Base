7659273557c49256eb45adb8bd48109befe13b4d
Jacob Dubose <jdubose@IT405.mr.preble.com>
2016-01-20 16:17:49 -0500

    Finished menu animations
    wooooo0ooo

---
+++ Build A Base/Assets/MagicAnimationScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicAnimationScript : MonoBehaviour {
+
+    [SerializeField] Animator CameraAnimator;
+    [SerializeField] Animator CanvasAnimator;
+
+
+    public void TriggerStart()
+    {
+        CameraAnimator.SetTrigger("DiveIn");
+        CanvasAnimator.SetTrigger("GoClicked");
+    }
+}
