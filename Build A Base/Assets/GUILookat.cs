4e5be2dc8c9b44fd848494f5ee8039df59eec9d2
theuglysweater <jmarks@IT103.mr.preble.com>
2016-01-25 14:58:12 -0500

    radial menu animated

---
+++ Build A Base/Assets/GUILookat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUILookat : MonoBehaviour {
+
+    [SerializeField]
+    Transform cameraRot;
+
+    [SerializeField]
+    Camera mainCamera;
+
+    [SerializeField]
+    float scaleing;
+
+    [SerializeField]
+    Transform placePos;
+
+    float guiScale;
+
+    void Update () {
+        guiScale = mainCamera.orthographicSize;
+        transform.rotation = cameraRot.rotation;
+        transform.localScale = new Vector3(guiScale / scaleing, guiScale / scaleing, guiScale / scaleing);
+        transform.position = placePos.position;
+    }
+}
