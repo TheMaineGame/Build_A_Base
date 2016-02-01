bc6aa56b6388c0d7b458dc435e03aab450043702
Alex Rediker <redikeralex@utc4me.org>
2016-02-01 15:49:46 -0500

    Alex's Stuff

---
+++ Build A Base/Assets/Scripts/SoundScripts/Mute.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mute : MonoBehaviour {
+	public GameObject mute;
+	public GameObject volume;
+
+	public void Sound () 
+	{
+		AudioListener.volume = 0;
+
+		mute.SetActive (true);
+		volume.SetActive (false);
+	}
+}
