271af80f7cd115aa78bed44beee39e37037a3377
theuglysweater <jmarks@IT403_REDONE.mr.preble.com>
2016-02-08 15:43:49 -0500

    Camera And

--- Assets/CameraControll.cs
+++ Assets/CameraControll.cs
@@ -62,6 +62,8 @@ public class CameraControll : MonoBehaviour {
     float screenWidth = Screen.width;
     float screenHeight = Screen.height;
 
+	bool canDrag = false;
+
     void Start()
 	{
         focusPoint = GameObject.FindGameObjectWithTag("FocusPoint");
@@ -82,25 +84,32 @@ public class CameraControll : MonoBehaviour {
 
     void Update()
 	{
+		if (Input.GetMouseButtonDown(panInt) || Input.GetMouseButtonDown(rotateInt)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 1000) && hit.transform.tag == "Floor") {
+				canDrag = true;
+			} else {
+				canDrag = false;
+			}
+		}
+
         float xpos = Input.mousePosition.x;
         float ypos = Input.mousePosition.y;
         Vector3 movement = new Vector3(0, 0, 0);
 
         curZoom = Mathf.Lerp(curZoom, currentZoom, Time.deltaTime * zoomSpeed);
 
-        //float fov = actuallCamera.fieldOfView;
+        float fov = actuallCamera.fieldOfView;       
 
-        //Debug.Log (camstate);
-       
-
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(screenWidth/2 , screenHeight/2));
-        RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 100))
-        {
-            //focusPoint.transform.position = hit.point;
-            focusPoint.transform.localPosition = Vector3.Lerp(focusPoint.transform.position, hit.point, Time.deltaTime * 3);
-            Debug.DrawLine(ray.origin, hit.point);
-        }
+//        Ray ray = Camera.main.ScreenPointToRay(new Vector3(screenWidth/2 , screenHeight/2));
+//        RaycastHit hit;
+//        if(Physics.Raycast(ray, out hit, 100))
+//        {
+//            //focusPoint.transform.position = hit.point;
+//            focusPoint.transform.localPosition = Vector3.Lerp(focusPoint.transform.position, hit.point, Time.deltaTime * 3);
+//            Debug.DrawLine(ray.origin, hit.point);
+//        }
 
         if(curZoom == currentZoom)
         {
@@ -114,7 +123,6 @@ public class CameraControll : MonoBehaviour {
         if (!cameraType) {
             actuallCamera.orthographicSize = Mathf.Lerp(actuallCamera.orthographicSize, currentZoom, Time.deltaTime * zoomSpeed);
 		} else {
-            //actuallCamera.fieldOfView = currentZoom * 5;
             actuallCameraObject.transform.localPosition = new Vector3(0, curZoom, -curZoom);
 		}
 
@@ -122,51 +130,26 @@ public class CameraControll : MonoBehaviour {
 
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 
-        if (Input.GetMouseButton(rotateInt))
-        {
-            yDeg += Input.GetAxis("Mouse X") * Time.deltaTime * actuallRotate;
-            fromRotation = cameraObject.transform.rotation;
-            toRotation = Quaternion.Euler(0, yDeg, 0);
-            cameraObject.transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * actuallRotate);
-        }
-
-        //if (Input.GetMouseButton(0))
-        //{
-        //    movement.x -= Input.GetAxis("Mouse X") * moveSpeed;
-        //    movement.z -= Input.GetAxis("Mouse Y") * moveSpeed;
-        //}
-
-        //movement = cameraObject.transform.TransformDirection(movement);
-        //movement.y = 0;
-        ////movement.y -= 
-
-        //Vector3 origin = cameraObject.transform.position;
-        //Vector3 destination = origin;
-        //destination.x += movement.x;
-        //destination.z += movement.z;
-        //destination.y += movement.y;
-
-        //if(origin != destination)
-        //{
-        //    transform.position = Vector3.MoveTowards(origin, destination, Time.deltaTime * moveSpeed * fovOrsize);
-        //}
-
-        if (Input.GetMouseButtonDown(panInt))
-        {
-            lastMouseX = Input.mousePosition.x;
-            lastMouseY = Input.mousePosition.y;
-        }
-        if (Input.GetMouseButton(panInt))
-        {
-            float deltaX = Input.mousePosition.x - lastMouseX;
-            float deltaY = Input.mousePosition.y - lastMouseY;
-            //fromPos = cameraObject.transform.localPosition;
-            //toPos = transform.right * deltaX + transform.forward * deltaY + toPos;
-            //cameraObject.transform.localPosition = Vector3.Lerp(fromPos, toPos, Time.deltaTime * moveSpeed);
-            cameraObject.transform.position = transform.forward * (deltaY * moveSpeed) + transform.right * (deltaX * moveSpeed) + cameraObject.transform.position;
-            lastMouseX = Input.mousePosition.x;
-            lastMouseY = Input.mousePosition.y;
-        }
+		if (canDrag) {
+			if (Input.GetMouseButton (rotateInt)) {
+				yDeg += Input.GetAxis ("Mouse X") * Time.deltaTime * actuallRotate;
+				fromRotation = cameraObject.transform.rotation;
+				toRotation = Quaternion.Euler (0, yDeg, 0);
+				cameraObject.transform.rotation = Quaternion.Lerp (fromRotation, toRotation, Time.deltaTime * actuallRotate);
+			}
+		
+			if (Input.GetMouseButtonDown (panInt)) {
+				lastMouseX = Input.mousePosition.x;
+				lastMouseY = Input.mousePosition.y;
+			}
+			if (Input.GetMouseButton (panInt)) {
+				float deltaX = Input.mousePosition.x - lastMouseX;
+				float deltaY = Input.mousePosition.y - lastMouseY;
+				cameraObject.transform.position = transform.forward * (deltaY * moveSpeed) + transform.right * (deltaX * moveSpeed) + cameraObject.transform.position;
+				lastMouseX = Input.mousePosition.x;
+				lastMouseY = Input.mousePosition.y;
+			}
+		}
     }
 
 	void CameraModeCheck(){
