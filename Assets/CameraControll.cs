68beae0d2655224d1fb889df0059fb24285a2fbf
theuglysweater <jmarks@IT403_REDONE.mr.preble.com>
2016-02-26 15:03:57 -0500

    Planets

--- Assets/CameraControll.cs
+++ Assets/CameraControll.cs
@@ -3,11 +3,41 @@ using System.Collections;
 
 public class CameraControll : MonoBehaviour {
 
-    [SerializeField]
+	public enum cameraMode {orthographic, perspective}
+    public enum rotateMode {LeftClick = 0, RightClick = 1, MiddleClick = 2}
+    public enum panMode {LeftClick = 0, RightClick = 1, MiddleClick = 2}
+
+	public cameraMode cameraState;
+    public rotateMode rotateButton;
+    public panMode panButton;
+
     GameObject cameraObject;
 
     [SerializeField]
+    GameObject actuallyCamera;
+
+    [SerializeField]
+    GameObject camHolder;
+
+    GameObject actuallCameraObject;
+
     Camera actuallCamera;
+
+    int rotateInt;
+
+    int panInt;
+
+    GameObject focusPoint;
+
+    [SerializeField]
+    Transform camPos;
+
+    [SerializeField]
+    float dragSpeed;
+
+    private Vector3 panOrigin;
+
+    private Vector3 dragOrigin;
 
     [SerializeField]
     float maxZoom;
@@ -21,11 +51,13 @@ public class CameraControll : MonoBehaviour {
     [SerializeField]
     float rotateSpeed;
 
-    [SerializeField]
+    float curZoom;
+
+    float actuallRotate;
+
     float moveSpeed;
 
-    [SerializeField]
-    float moveZoomMultiplier = 0.002f;
+    float moveZoomMultiplier;
 
     Quaternion fromRotation;
     Quaternion toRotation;
@@ -39,45 +71,150 @@ public class CameraControll : MonoBehaviour {
 
     float currentZoom;
 
+	bool cameraType;
+
+    float fovOrsize;
+
+    float screenWidth = Screen.width;
+    float screenHeight = Screen.height;
+
+	bool canDrag = false;
+
+    bool dragging;
+
+    float width;
+    float height;
+    float boundary = 1;
+
     void Start()
-    {
-        currentZoom = actuallCamera.orthographicSize;
+	{
+        width = Screen.width;
+        height = Screen.height;
+        focusPoint = GameObject.FindGameObjectWithTag("FocusPoint");
+        transform.parent.position = new Vector3(0, 0, 0);
+        panInt = (int)panButton;
+        rotateInt = (int)rotateButton;
+        cameraObject = this.gameObject;
+        actuallCameraObject = this.gameObject.transform.GetChild(0).gameObject;
+        actuallCamera = GetComponentInChildren<Camera>();
+        CheckZoom();
+		CameraModeCheck ();
+        if (panInt == rotateInt)
+        {
+            throw new System.Exception("Buttons should not be the same");
+        }
+
+        //groundPlane = Plane (groundNormal, groundPoint);
     }
 
     void Update()
-    {
+	{
+        float xMouse = Input.mousePosition.x;
+        float yMouse = Input.mousePosition.y;
+
+		if (Input.GetMouseButtonDown(panInt) || Input.GetMouseButtonDown(rotateInt)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 1000) && hit.transform.tag == "Floor") {
+				canDrag = true;
+			} else {
+				canDrag = false;
+			}
+		}
+
+        float xpos = Input.mousePosition.x;
+        float ypos = Input.mousePosition.y;
+        Vector3 movement = new Vector3(0, 0, 0);
+
+        curZoom = Mathf.Lerp(curZoom, currentZoom, Time.deltaTime * zoomSpeed);
+
+        float fov = actuallCamera.fieldOfView;       
+
+//        Ray ray = Camera.main.ScreenPointToRay(new Vector3(screenWidth/2 , screenHeight/2));
+//        RaycastHit hit;
+//        if(Physics.Raycast(ray, out hit, 100))
+//        {
+//            //focusPoint.transform.position = hit.point;
+//            focusPoint.transform.localPosition = Vector3.Lerp(focusPoint.transform.position, hit.point, Time.deltaTime * 3);
+//            Debug.DrawLine(ray.origin, hit.point);
+//        }
+
+        if(curZoom == currentZoom)
+        {
+            CheckZoom();
+        }
 
         moveSpeed = moveZoomMultiplier * -currentZoom;
 
-        actuallCamera.orthographicSize = currentZoom;
+        actuallRotate = rotateSpeed * 100;
+
+        if (!cameraType) {
+            actuallCamera.orthographicSize = Mathf.Lerp(actuallCamera.orthographicSize, currentZoom, Time.deltaTime * zoomSpeed);
+		} else {
+            actuallCameraObject.transform.localPosition = new Vector3(0, curZoom, -curZoom);
+		}
 
         currentZoom -= (Input.GetAxis("Mouse ScrollWheel") * zoomSpeed);
 
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
 
-        if (Input.GetMouseButton(1))
-        {
-            yDeg += Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
-            fromRotation = cameraObject.transform.rotation;
-            toRotation = Quaternion.Euler(0, yDeg, 0);
-            cameraObject.transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * rotateSpeed);
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            lastMouseX = Input.mousePosition.x;
-            lastMouseY = Input.mousePosition.y;
-        }
-        if (Input.GetMouseButton(0))
-        {
-            float deltaX = Input.mousePosition.x - lastMouseX;
-            float deltaY = Input.mousePosition.y - lastMouseY;
-            //fromPos = cameraObject.transform.localPosition;
-            //toPos = transform.right * deltaX + transform.forward * deltaY + toPos;
-            //cameraObject.transform.localPosition = Vector3.Lerp(fromPos, toPos, Time.deltaTime * moveSpeed);
-            cameraObject.transform.position = transform.forward * (deltaY * moveSpeed) + transform.right * (deltaX * moveSpeed) + cameraObject.transform.position;
-            lastMouseX = Input.mousePosition.x;
-            lastMouseY = Input.mousePosition.y;
+		if (canDrag) {
+			if (Input.GetMouseButton (rotateInt)) {
+				yDeg += Input.GetAxis ("Mouse X") * Time.deltaTime * actuallRotate;
+				fromRotation = camHolder.transform.rotation;
+				toRotation = Quaternion.Euler (0, yDeg, 0);
+				camPos.rotation = Quaternion.Lerp (fromRotation, toRotation, Time.deltaTime * actuallRotate);
+			}
+
+            //if (Input.GetMouseButtonDown(panInt))
+            //{
+            //  dragOrigin = Input.mousePosition;
+            //}
+
+            //if (Input.GetMouseButton(panInt))
+            //{
+            //    Vector3 delta = Input.mousePosition - dragOrigin;
+            //    transform.Translate(delta.x * dragSpeed, 0 ,delta.z * dragSpeed, 0);
+            //    dragOrigin = Input.mousePosition;
+            //}
+
+            //original Panning
+            if (Input.GetMouseButtonDown(panInt))
+            {
+                lastMouseX = Input.mousePosition.x;
+                lastMouseY = Input.mousePosition.y;
+            }
+            if (Input.GetMouseButton(panInt))
+            {
+                float deltaX = Input.mousePosition.x - lastMouseX;
+                float deltaY = Input.mousePosition.y - lastMouseY;
+                cameraObject.transform.position = transform.forward * (deltaY * moveSpeed) + transform.right * (deltaX * moveSpeed) + cameraObject.transform.position;
+                lastMouseX = Input.mousePosition.x;
+                lastMouseY = Input.mousePosition.y;
+            }
         }
     }
 
+	void CameraModeCheck(){
+		if (cameraState == cameraMode.orthographic) {
+			actuallCamera.orthographic = true;
+			currentZoom = actuallCamera.orthographicSize;
+			moveZoomMultiplier = 0.002f;
+            fovOrsize = actuallCamera.orthographicSize * 2;
+            moveZoomMultiplier = screenWidth / screenHeight / 800;
+            cameraType = false;
+		} else {
+			actuallCamera.orthographic = false;
+			currentZoom = actuallCamera.fieldOfView;
+			moveZoomMultiplier = 0.003f;
+            fovOrsize = actuallCamera.fieldOfView;
+            moveZoomMultiplier = (screenWidth / screenHeight / 1200);
+            cameraType = true;
+		}
+	}
+
+    void CheckZoom()
+    {
+        curZoom = currentZoom;
+    }
 }
