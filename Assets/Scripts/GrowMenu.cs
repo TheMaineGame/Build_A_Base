435cb96cfbd4d18f49dee6c5f8211c31efbaae7e
Austin Fink <afink@utc4me.org>
2016-02-29 16:21:39 -0500

    The Menu Prefab
    Added the menu prefab with Itween

---
+++ Assets/Scripts/GrowMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowMenu : MonoBehaviour {
+	
+	void Start ()
+    {
+        iTween.ScaleTo(gameObject, iTween.Hash("time", 2, "x", 1.25, "y", 1.25, "easetype", "linear"));
+    }
+}
---
+++ Assets/Scripts/MenTween.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenTween : MonoBehaviour {
+
+	public void Pop(GameObject target)
+    {
+        iTween.ScaleFrom(target, iTween.Hash("x", 1, "y", 1));
+    }
+}
