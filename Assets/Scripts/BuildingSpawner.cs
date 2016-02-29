2db56858bed0eb441afa21e80d13df051f1af740
Alex Rediker <redikeralex@utc4me.org>
2016-02-29 16:31:04 -0500

    ObjectSpawnerBegining
    NOOOOPE

---
+++ Assets/Scripts/BuildingSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class BuildingSpawner : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler 
+{
+	[SerializeField] GameObject building;
+	private bool pointerDown = false;
+
+	void Start () 
+	{
+
+	}
+	
+	void Update () 
+	{
+	
+	}
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		pointerDown = true;
+	}
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		pointerDown = false;
+	}
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		pointerDown = false;
+	}
+}
